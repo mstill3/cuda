7497f3a8d80db90ac832767369134961be6ad35d
Matt <stillwell006@gmail.com>
2019-09-24 15:54:08 +0000

    Finish flashlight code and add glut examples

--- flashlight/kernel.cu
+++ flashlight/kernel.cu
@@ -1,68 +1,44 @@
-#include <stdio.h>    // for printf
+#include "kernel.h"
 
-#define W 500          // constant, width of image
-#define H 500          // constant, height of image
 #define TX 32          // constant, num threads per block along x axis
 #define TY 32          // constant, num threads per block along y axis
 
 
 // Returns val of element unless greater than 255 then just returns 255
-__device__ 
-float clip(int n)
-{
-    return n > 255 ? 255 : (n < 0 ? 0 : n);
+__device__
+unsigned char clip(int n) 
+{ 
+    return n > 255 ? 255 : (n < 0 ? 0 : n); 
 }
 
-
-__global__ 
-void distanceKernel(uchar4 *d_out, int w, int h, int2 pos)    
+__global__
+void distanceKernel(uchar4 *d_out, int w, int h, int2 pos) 
 {
-    const int c = blockIdx.x * blockDim.x + threadIdx.x;
-    const int r = blockIdx.y * blockDim.y + threadIdx.y;
-    const int i = r * w + c;
+    const int c = blockIdx.x*blockDim.x + threadIdx.x;
+    const int r = blockIdx.y*blockDim.y + threadIdx.y;
+    const int i = c + r * w;   // 1D indexing
 
-    if((c >= w) || (r >= h))
-    {
-        return;
+    if ((c >= w) || (r >= h))
+    { 
+        return; // Check if within image bounds
     }
 
     // compute distance (in pixel spacings)
-    const int d = sqrtf((c - pos.x) * (c - pos.x) + (r - pos.y) * (r - pos.y));
+    const int dist = sqrtf((c - pos.x)*(c - pos.x) + 
+                           (r - pos.y)*(r - pos.y));
 
     // convert distance to intensity value on interval (0, 255)
-    const unsigned char intensity = clip(255 - d);
+    const unsigned char intensity = clip(255 - dist);
 
     d_out[i].x = intensity;   // red channel 
     d_out[i].y = intensity;   // green channel 
     d_out[i].z = 0;           // blue channel 
-    d_out[i].z = 255;         // fully opaque 
-
-    //printf("i = %2d: dist from %f to %f is %f.\n", i, ref, x, d_out[i]);
+    d_out[i].w = 255;         // fully opaque 
 }
 
-// Auto run main method 
-int main()
+void kernelLauncher(uchar4 *d_out, int w, int h, int2 pos) 
 {
-
-    uchar4 *out = (uchar4*) calloc(W * H, sizeof(uchar4));
-    uchar4 *d_out; // pointer for device array
-    cudaMallocManaged(&d_out, W * H * sizeof(uchar4));
-
-    const int2 pos = {0, 0}; // set reference position
     const dim3 blockSize(TX, TY);
-    const int bx = (W + TX - 1)/ TX;
-    const int by = (W + TY - 1)/ TY;
-    const dim3 gridSize = dim3(bx, by);
-
-    // launch kernel to compute and store distance vals
-    distanceKernel<<<gridSize, blockSize>>>(d_out, W, H, pos);
-    
-    // Copy results to host
-    cudaMemcpy(out, d_out, W * H * sizeof(uchar4), cudaMemcpyDeviceToHost);
-
-    cudaFree(d_out);
-    free(out);
-
-    return 0;
-
+    const dim3 gridSize = dim3((w + TX - 1)/TX, (h + TY - 1)/TY);
+    distanceKernel<<<gridSize, blockSize>>>(d_out, w, h, pos);
 }
